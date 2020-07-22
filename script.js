49a3200fc07a75ba786caa3b8a16cac82a924596
Timothy Munene <munenegicharu@gmail.com>
2020-07-22 10:34:15 +0300

    Add link to kata challenge

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+// Function that returns the sum of all the multiples
+// of 3 or 5 below the number passed in.
+// Code wars kata link: https://www.codewars.com/kata/514b92a657cdc65150000006/train/javascript
+
+
 function solution(number){
     let sum = 0;
     
